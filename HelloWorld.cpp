46bd91e179ff09b8f20bd8c265184b7fff66c920
DEATHART101 <z12603@163.com>
2018-04-22 16:26:59 +0800

    Echo branch
    Echo

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	char buf[512];
+	cin >> buf;
+	cout << "You entered: " << buf << endl;
+
+	cout << "Hello World" << endl;
+
+	return 0;
+	
+}
